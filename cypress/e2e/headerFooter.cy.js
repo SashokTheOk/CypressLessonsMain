0479a5a0c9a4873a5617e9ec39fbd9f42fdbbd6b
SashOK <qa.shvets@gmail.com>
2025-07-04 18:24:55 +0300

    cypressdasdasd

---
+++ cypress/e2e/headerFooter.cy.js
@@ -0,0 +1,30 @@
+describe('Header and Footer Elements', () => {
+    before(() => {
+        cy.visit('https://qauto.forstudy.space/', {
+            auth: {
+                username: 'guest',
+                password: 'welcome2qauto',
+            },
+        });
+    });
+
+    it('should find all header buttons', () => {
+        cy.get('app-header').within(() => {
+            cy.get('button').should('have.length.at.least', 1);
+            cy.get('button').each(($btn) => {
+                cy.wrap($btn).should('be.visible');
+            });
+        });
+    });
+
+    it('should find all footer links and buttons', () => {    
+        cy.get('app-home').find('#contactsSection').as('contactsSection');    
+        cy.get('@contactsSection').scrollIntoView();      
+        cy.get('@contactsSection').within(() => { 
+            cy.get('a, button').should('have.length.at.least', 1);
+            cy.get('a, button').each(($el) => {
+                cy.wrap($el).should('be.visible');
+            });
+        });
+    });
+});
